90d87db27dbfc6eedf7dc2768f1553237952fe5e
Rio Ananta <rioananta1@gmail.com>
2023-07-21 07:41:20 +0700

    fix minor bugs

--- src/app/api/(bahan)/tambah_bahan/route.ts
+++ src/app/api/(bahan)/tambah_bahan/route.ts
@@ -10,22 +10,32 @@ interface RequestBody {
 async function handler(request: NextRequest) {
   const body: RequestBody = await request.json();
 
-  const currentBahan = await db.bahan.findMany()
+  const currentBahan = await db.bahan.aggregate({
+    _max: {
+      ID_BAHAN: true,
+    },
+  });
+
+  const urutan =
+    currentBahan._max.ID_BAHAN?.substring(
+      currentBahan._max.ID_BAHAN.length,
+      currentBahan._max.ID_BAHAN.length - 3
+    ) ?? "0";
 
   function kodifikasiBahan(jumlah: number) {
     const prefix = "B";
-    if (jumlah >= 10) {
+    if (jumlah > 9) {
       return `${prefix}0${jumlah}`;
-    } else if (jumlah >= 100) {
+    } else if (jumlah > 99) {
       return `${prefix}${jumlah}`;
     } else {
-      return `${prefix}00${jumlah + 1}`;
+      return `${prefix}00${jumlah}`;
     }
   }
 
   const bahanBaru = await db.bahan.create({
     data: {
-      ID_BAHAN: kodifikasiBahan(currentBahan.length + 1),
+      ID_BAHAN: kodifikasiBahan(parseInt(urutan) + 1),
       NAMA_BAHAN: body.namaBahan,
       STOCK_BAHAN: body.stockBahan,
       UNIT_BAHAN: body.unitBahan,
